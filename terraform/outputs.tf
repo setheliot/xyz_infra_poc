343d7cfbf2f712dce9fd813f2d0ef837a338dc89
Seth Eliot <seth@setheliot.com>
2024-09-09 09:55:04 -0700

    Add AWS region to output. Add cost estimate to README

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,6 +5,11 @@ output "vpc_id" {
 }
 
 # Output the EKS cluster details
+output "eks_cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
 output "eks_cluster_endpoint" {
   description = "EKS Cluster API Endpoint"
   value       = module.eks.cluster_endpoint
@@ -13,5 +18,11 @@ output "eks_cluster_endpoint" {
 output "eks_cluster_arn" {
   description = "EKS Cluster ARN"
   value       = module.eks.cluster_arn
+}
+
+# Output the AWS Region
+output "aws_region" {
+  description = "AWS region"
+  value       = var.aws_region
 }
 
